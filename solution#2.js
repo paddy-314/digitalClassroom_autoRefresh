920ba72c84a935df1acabd58c38ada2b56ca4307
paddy-314 <100515405+paddy-314@users.noreply.github.com>
2022-07-08 11:42:58 +0200

    Update solution#2.js

--- solution#2.js
+++ solution#2.js
@@ -1,13 +1,14 @@
-javascript: document.getElementsByTagName("body")[0].innerHTML = "<iframe id=\"testFrame\" src=\"" + window.location.toString() + "\" style=\"position: absolute; top:0; left:0; right:0; bottom:0; width:100%; height:100%;\"><\/iframe>";
+javascript: document.getElementsByTagName("body")[0].innerHTML = "<iframe id=\"iFrame\" src=\"" + window.location.toString() + "\" style=\"position: absolute; top:0; left:0; right:0; bottom:0; width:100%; height:100%;\"><\/iframe>";
 reloadTimer = setInterval(function() {
-    var attr = $("div.run-container").children("button").attr('disabled');
-    $("div.run-container").attr("id", "containerId")
-    var id = $("div.run-container").children("button").attr('id');
+    var attr = $("#iFrame").contents().find("div.run-container").children("button").attr('disabled');
+    $("#iFrame").contents().find("div.run-container").attr("id", "containerId")
+    var id = $("#iFrame").contents().find("div.run-container").children("button").attr('id');
     if (typeof attr !== 'undefined' && attr !== false) {
         console.log("not open yet, refreshing");
-        document.getElementById("testFrame").src = document.getElementById("testFrame").src
+        document.getElementById("iFrame").src = document.getElementById("iFrame").src
     }
-    else {
+    else if (typeof attr == 'object' && attr == true){ //make sure the object is loaded
+        console.log(attr)
         var speak = new SpeechSynthesisUtterance("Der Raum ist offen, es geht los!");
         speak.lang = "de-DE";
         speechSynthesis.speak(speak);
